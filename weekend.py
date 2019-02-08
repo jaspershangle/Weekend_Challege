e3a8daa6f2184be55246be8bef5c04a35576d75a
jaspershangle <jasper@shangle.com>
2019-02-08 16:15:34 -0600

    Creating repository - Weekend Challenge

---
+++ weekend.py
@@ -0,0 +1,30 @@
+# Weekend Algorithm 
+
+# Write a function that reverses a string without affecting special caracters
+# "!!aPples_Ar3__Gr8!"
+# "!!rG3rA_sel__pPa!
+
+# Rules
+# No built in methods
+
+# Challenge
+# Do this in Ruby
+import re
+
+def reverse_string_special(input_string):
+    forward_list = list(input_string)
+    answer_list = []
+    for letter in forward_list:
+        if 
+    for i in forward_list[::-1]:
+
+        # print(letter)
+        # if re.match("^[a-zA-Z0-9_]*$", letter):
+        #     print('letter')
+        # else:
+        #     print('Not a letter')
+        # print('\n')
+            
+
+
+reverse_string_special('!!aPples_Ar3__Gr8!')
