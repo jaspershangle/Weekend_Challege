6d495f0b73d5f1bc48b6a50d99c0351c188e1ca6
jaspershangle <jasper@shangle.com>
2019-02-08 17:30:00 -0600

    Jasper Python solution

--- weekend.py
+++ weekend.py
@@ -12,19 +12,19 @@
 import re
 
 def reverse_string_special(input_string):
-    forward_list = list(input_string)
-    answer_list = []
-    for letter in forward_list:
-        if 
-    for i in forward_list[::-1]:
+    input_list = list(input_string)
+    left = 0
+    right = len(input_list) - 1 
+    regex = '[a-zA-Z0-9]'
+    while left < right:
+        if bool(re.match(regex, input_list[left])) == False:
+            left += 1
+        elif bool(re.match(regex, input_list[right])) == False:
+            right -= 1
+        else:
+            input_list[left], input_list[right] = input_list[right], input_list[left]
+            left += 1
+            right -= 1
+    return "".join(input_list)
 
-        # print(letter)
-        # if re.match("^[a-zA-Z0-9_]*$", letter):
-        #     print('letter')
-        # else:
-        #     print('Not a letter')
-        # print('\n')
-            
-
-
-reverse_string_special('!!aPples_Ar3__Gr8!')
+print(reverse_string_special('!!aPples_Ar3__Gr8!'))
